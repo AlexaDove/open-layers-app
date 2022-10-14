c67282a7ab6f6c78c05653f7ca796a1368634052
AlexaDove <alesya.g99@gmail.com>
2022-10-14 14:22:39 +0700

    base

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,7 @@
-cd./dist
+cd ./dist
 git init
 git checkout -B master
+git add -A
 git commit -m "latest site deloy"
 git push -f https://github.com/AlexaDove/open-layers-app.git master:www_dist
+cd ..
